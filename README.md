4635808f174775caf6480fff4e6dfa1f18b708ed
Luis Fernando Neu <luis.neu@gmail.com>
2025-03-05 23:05:06 -0300

    tf to create eks cluster in AWS

---
+++ README.md
@@ -0,0 +1,2 @@
+# POC
+terraform and eks
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  default = "us-west-2"
+}
+variable "aws_aza" {
+  default = "us-west-2a"
+}
+
+variable "aws_azb" {
+  default = "us-west-2b"
+}
