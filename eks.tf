443c77988512690f7b0de60415233c55e334c04f
Luis Fernando Neu <luisneu@gmail.com>
2025-04-06 13:23:40 -0300

    Update eks.tf code cleanup

--- eks.tf
+++ eks.tf
@@ -5,7 +5,8 @@ module "eks" {
   cluster_name                   = local.name
   cluster_version = "1.32"
   cluster_endpoint_public_access = true
-  # Optional: Adds the current caller identity as an administrator via cluster access entry
+  
+# Adds the current caller identity as an administrator via cluster access entry
   enable_cluster_creator_admin_permissions = true
 
 
@@ -24,14 +25,6 @@ module "eks" {
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.intra_subnets
-
-  # EKS Managed Node Group(s)
-  # eks_managed_node_group_defaults = {
-  #   ami_type       = "AL2_x86_64"
-  #   instance_types = ["m5.large"]
-
-  #   attach_cluster_primary_security_group = true
-  # }
 
   eks_managed_node_groups = {
     amc-cluster-wg = {
